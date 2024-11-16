ff7a0573d381b11e05707fe8da76357796f82e45
helicoptair <contato@helicoptair.com.br>
2024-11-16 13:44:51 -0300

    traducao

--- src/app/shared-components/header-carousel/header-carousel.component.ts
+++ src/app/shared-components/header-carousel/header-carousel.component.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header-carousel',
@@ -13,6 +14,7 @@ import { CurrencyPipe } from '@angular/common';
   imports: [
     FlexLayoutModule,
     MatButtonModule,
+    TranslateModule,
     MatIconModule,
     SwiperModule,
     MatCardModule,
